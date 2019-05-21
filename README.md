999f3095e57b43886971dbfe7cc641e87c0dd09a
P. Dockhorn <dockhorn@innoroute.com>
2019-05-21 19:00:07 +0200

    add install and conf eth

--- README.md
+++ README.md
@@ -9,3 +9,8 @@ activates HID ``dwc2`` module, sets ``hid.service`` and install ``scansend``
 $ sudo ./setup.sh
 ```
 
+% START ETHERNET (END HID)
+% sudo echo "" > UDC; sleep 1; sudo rm -v /sys/kernel/config/usb_gadget/g1/configs/c.1/hid.usb0; sleep 1;sudo modprobe g_ether;
+
+% START HID (END ETHERNET)
+% sudo rmmod g_ether; sleep 1; sudo hid;
--- setup.sh
+++ setup.sh
@@ -3,6 +3,9 @@
 # Test if is Root
 if [[ $(id -u) -ne 0 ]] ; then echo "Please run as root" ; exit 1 ; fi
 
+# Install tools
+apt-get install dnsmasq adb
+
 # Enable dwc2 on the Pi
 echo "dtoverlay=dwc2" | sudo tee -a /boot/config.txt
 
@@ -11,13 +14,17 @@ echo "dwc2" | sudo tee -a /etc/modules
 
 # Install service
 cp hid.service /lib/systemd/system/
+cp eth.service /lib/systemd/system/
 
 # Install startscript
 cp hid /usr/bin/
 chmod +x /usr/bin/hid
+cp eth /usr/bin/
+chmod +x /usr/bin/eth
 
-# Enable service
+# Enable/Disable service
 systemctl enable hid.service
+systemctl disable eth.service
 
 # Compile scansend
 gcc -Wall -Werror scansend.c -o scansend
@@ -25,6 +32,14 @@ gcc -Wall -Werror scansend.c -o scansend
 # Install script
 cp scansend /usr/bin/
 chmod +x /usr/bin/scansend
+
+# Config ETH
+p1="/etc/dhcpcd.conf"
+p2="/etc/dnsmasq.conf"
+[ -f $p1 ] && mv $p1 $p1.old
+[ -f $p2 ] && mv $p2 $p2.old
+echo "interface usb0\nstatic ip_address=192.168.66.1/24" | tee -a $p1
+echo "interface=usb0\ndhcp-range=192.168.66.20,192.168.66.100,255.255.255.0,12h" | tee -a $p2
 
 # Reboot
 read -p "Reboot necessary. Perform now?" -n 1 -r
